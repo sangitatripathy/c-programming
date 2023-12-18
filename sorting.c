a8f6b6a20ff5f2a476f7327956cab4ba2a17dd65
sangitatripathy <147959850+sangitatripathy@users.noreply.github.com>
2023-12-18 22:29:42 +0530

    Update sorting.c

--- sorting.c
+++ sorting.c
@@ -1,1 +1,25 @@
-
+1.//WAP to sort an array in ascending order//
+#include<stdio.h>
+int main(){
+    int i,n,j;
+    printf("enter the size of array");
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++){
+        printf("enter the element %d :",i+1);
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++){
+        for(j=0;j<n-1-i;j++){
+            if (arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    printf("\n");
+    for(i=0;i<n;i++){
+        printf("%d",arr[i]);
+    }
+}
